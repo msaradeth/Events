232c3587038ab7af81252986ccda9425cb936aca
Mike Saradeth <mikesaradeth@yahoo.com>
2019-06-13 01:34:40 -0700

    work in progress

--- Events/List/ListVC.swift
+++ Events/List/ListVC.swift
@@ -8,72 +8,52 @@
 
 import UIKit
 
-class ListVC: UIViewController {
+class ListVC: UICollectionViewController {
     var viewModel: ListViewModel
-    lazy var collectionView: UICollectionView = {
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .white
-        collectionView.register(UINib(nibName: "ListCell", bundle: nil), forCellWithReuseIdentifier: ListCell.cellIdentifier)
-        collectionView.dataSource = self
-        collectionView.delegate = self
-        return collectionView
-    }()
  
     init(title: String, viewModel: ListViewModel) {
-        self.viewModel = viewModel        
-        super.init(nibName: nil, bundle: nil)
+        self.viewModel = viewModel
+        super.init(collectionViewLayout: UICollectionViewFlowLayout())
         self.title = title
-        self.view.backgroundColor = .white
+        self.collectionView.backgroundColor = .white
+        self.collectionView.register(UINib(nibName: "ListCell", bundle: nil), forCellWithReuseIdentifier: ListCell.cellIdentifier)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupCollectionView()
         viewModel.loadData { [weak self] in
             DispatchQueue.main.async {
                 self?.collectionView.reloadData()
             }
         }
     }
-    
-    func setupCollectionView() {
-        view.addSubview(collectionView)
-        collectionView.fillsuperview()
-    }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
 }
 
-extension ListVC: UICollectionViewDataSource {
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1
-    }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print(viewModel.items.count)
+extension ListVC {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.items.count
     }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ListCell.cellIdentifier, for: indexPath) as! ListCell
         cell.configure(item: viewModel[indexPath], index: indexPath.row, delegate: viewModel)
         return cell
     }
-}
-
-extension ListVC: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
     }
+    
 }
 
 extension ListVC: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-//         return CGSize(width: collectionView.bounds.width, height: 44)
-        return CGSize(width: 100, height: 100)
+
+         return CGSize(width: collectionView.bounds.width, height: 200)
     }
 }
 
